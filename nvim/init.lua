4c4ae485f382f2caf83df00ff8a023985dbdec46
majora <111635357+belgas023@users.noreply.github.com>
2026-06-09 12:54:35 +0200

    lsp + autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+require('autocompletion')
+
 -- general options
 vim.o.number = true -- line number
 vim.o.relativenumber = true -- relative line number
@@ -14,33 +16,36 @@ vim.o.laststatus = 3
 vim.o.undofile = true
 vim.o.cmdheight = 0
 vim.g.netrw_banner = 0
+vim.o.autocomplete = true
 
 
 
 -- plugin management
 vim.pack.add {
 				'https://github.com/nvim-mini/mini.icons', -- icons
-				'https://github.com/saghen/blink.cmp', -- auto completion
 				'https://github.com/neovim/nvim-lspconfig', -- official lsp plugin
 				'https://github.com/vague-theme/vague.nvim', -- colorscheme
 				'https://github.com/nvim-lualine/lualine.nvim', -- bar
 				'https://github.com/nvim-mini/mini.pick' -- fzf/file switcher
 }
 
-vim.lsp.enable({"lua_ls"})
 vim.cmd('colorscheme vague')
 require('lualine').setup()
 require('mini.pick').setup()
 require('mini.icons').setup()
---require('blink.cmp').setup({
-				--keymap = { preset = default },
-				--appearence = { nerd_font_variant = 'mono'},
-				--completion = { documentation = { auto_show = false }	},
-				--sources = { default = {'lsp', 'path', 'snippets', 'buffer'}},
-				--fuzzy = { implementation = "prefer_rust"}
---})
 
+-- lsp setup
+vim.lsp.enable('clangd')
+vim.lsp.enable('pylsp')
+
+-- diagnotic
+vim.diagnostic.config({
+	virtual_text = true,
+	virtual_lines = true
+})
 
 -- keymaps
 vim.keymap.set('n', 'Q', '<cmd>Pick files<CR>')
+vim.keymap.set('n', 'W', '<cmd>lua vim.diagnostic.open_float()<CR>')
 
+-- autocompletion
